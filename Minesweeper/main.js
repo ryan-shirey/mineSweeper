7fe7675de649ae9bd14b3b9b02303f8386e032dd
ryan-shirey <66448698+ryan-shirey@users.noreply.github.com>
2020-06-07 15:50:11 -0600

    Bug fix and mine proportion adjustment

--- Minesweeper/main.js
+++ Minesweeper/main.js
@@ -1,11 +1,13 @@
-gameBoard = new mineSweeper(10, 10, 25);
+gameBoard = new mineSweeper(10, 10, 10);
 clickedSet = new Set();
+suspectMines = new Set();
 function startGame(height, width, numMines) {
 
     this.gameBoard = new mineSweeper(height,width, numMines);
     this.clickedSet = new Set();
     const mineField = document.querySelector("#mineField");
     mineField.textContent = '';
+    document.querySelector("#statusText").textContent = '';
     let rowString = '25px';
     for (let i = 1; i < width; i++) {
         rowString = rowString + ' 25px';
@@ -22,23 +24,51 @@ function startGame(height, width, numMines) {
     
 }
 function onClickDelegator(event) {
+    console.log(event);
     let target = event.target.closest('div');
+
     if (target.classList.contains("mine")) {
-        triggerMine(target);
+        let mineID = parseInt(target.id);
+        if (event.shiftKey) {
+            if (!suspectMines.has(mineID) && !clickedSet.has(mineID)) {
+                suspectMines.add(mineID);
+                target.style.backgroundColor = "purple";
+            }
+            else if (suspectMines.has(mineID) && !clickedSet.has(mineID)) {
+                suspectMines.delete(mineID);
+                target.style.backgroundColor = "aqua";
+            }
+        }
+        else {
+            triggerMine(target);
+        }
     }
+}
+function endgame() {
+    let textBox = document.querySelector("#statusText")
+    textBox.textContent = "Kaboom, You Lose!";
+    reveal();
 }
 function reveal() {
     const mineField = document.querySelector("#mineField");
     for (i = 0; i < gameBoard.size; i++) {
         mine = mineField.children[i];
-        triggerMine(mine);
+        mineVal = gameBoard.mineCheck(parseInt(mine.id));
+        if (mineVal == -1) {
+            mine.style.backgroundColor = "red";
+        }
+        else {
+            mine.style.backgroundColor = "green";
+
+            mine.textContent = mineVal;
+        }
     }
 }
 function triggerMine(mine) {
-    clickedSet.add(mine);
+    clickedSet.add(parseInt(mine.id));
     var mineVal = gameBoard.mineCheck(parseInt(mine.id));
     if (mineVal == -1) {
-        mine.style.backgroundColor = "red";
+        endgame();
     }
     else {
         mine.style.backgroundColor = "green";
@@ -54,7 +84,7 @@ function triggerMine(mine) {
                 neighborID = parseInt(mine.id) + i + gameBoard.width * j;
                 if (neighborID >= 0 && neighborID < gameBoard.size) {
                     neighborMine = document.getElementById(String(neighborID));
-                    if (!clickedSet.has(neighborMine)) {
+                    if (!clickedSet.has(parseInt(neighborMine.id))) {
                         triggerMine(neighborMine);
                     }
                 }
